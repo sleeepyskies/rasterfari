75bdeb03788749c401c855f2c0e957796bf00aea
skysw <skwa00001@stud.uni-saarland.de>
2024-11-21 21:15:08 +0100

    link sdl2

--- src/rasterfari.cpp
+++ src/rasterfari.cpp
@@ -1,7 +1,10 @@
+#define SDL_MAIN_HANDLED
+
 #include "mathy.hpp"
 #include "model.hpp"
 #include "objparser.hpp"
 #include "tgaimage.hpp"
+#include <SDL.h>
 #include <iostream>
 
 const auto white = TGAColor(255, 255, 255, 255);
@@ -40,7 +43,7 @@ void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
     }
 }
 
-int main(int argc, char **argv) {
+void renderSumn() {
     // make image
     TGAImage img = TGAImage(W, H, TGAImage::Format::RGB);
 
@@ -50,7 +53,6 @@ int main(int argc, char **argv) {
     if (!oParser.loadObj(
             "D:/dev/c++ projects/graphics/renderer/models/crate.obj", model)) {
         std::cout << "fail lol" << std::endl;
-        return 1;
     }
 
     // render the model, disregarding the z dimension
@@ -87,7 +89,44 @@ int main(int argc, char **argv) {
 
     img.flip_vertically();
     img.write_tga_file("output.tga");
-    return 0;
+}
+
+/**
+ * Entry point of the renderer.
+ */
+int main(int argc, char **argv) {
+    // Initialize SDL
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        std::cerr << "SDL could not initialize! SDL_Error: " << SDL_GetError()
+                  << std::endl;
+        return -1;
+    }
+
+    // Create a window
+    SDL_Window *window =
+        SDL_CreateWindow("SDL2 Window",           // window title
+                         SDL_WINDOWPOS_UNDEFINED, // initial x position
+                         SDL_WINDOWPOS_UNDEFINED, // initial y position
+                         800,                     // width, in pixels
+                         600,                     // height, in pixels
+                         SDL_WINDOW_SHOWN         // window flags
+        );
+
+    if (window == nullptr) {
+        std::cerr << "Window could not be created! SDL_Error: "
+                  << SDL_GetError() << std::endl;
+        SDL_Quit();
+        return -1;
+    }
+
+    // Wait for 5 seconds
+    SDL_Delay(5000);
+
+    // Destroy the window
+    SDL_DestroyWindow(window);
+
+    // Quit SDL subsystems
+    SDL_Quit();
 
     return 0;
 }
